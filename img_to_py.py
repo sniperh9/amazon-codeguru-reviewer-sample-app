a21480470d21c48232420970b70d947f4e18c8ff
sniperh9 <87074551+sniperh9@users.noreply.github.com>
2024-05-23 14:30:12 +0900

    Update img_to_py.py

--- img_to_py.py
+++ img_to_py.py
@@ -1,7 +1,7 @@
 from wx.tools import img2py
  
 image_file = "screen img/im4.png"
-python_file = 'im4.py'
+python_file = 'im45.py'
  
 img2py.img2py(image_file=image_file, python_file=python_file,
               imgName='get_python_im4', icon=True)
